708e42f3b5a2960f43988323f3b73075d533f463
Ziad Abouelfarah <zain.fake02@gmail.com>
2023-04-11 00:09:21 +0000

    feat: add img holder ze3ma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { NavBar } from "@/components";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -12,6 +12,15 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <NavBar />
+      <div className="h-[calc(100vh_-_64px)] w-full">
+        <img
+          src={
+            "https://images.unsplash.com/photo-1583373834259-46cc92173cb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+          }
+          alt="img"
+          className="object-cover w-full h-full"
+        />
+      </div>
     </>
   );
 }
