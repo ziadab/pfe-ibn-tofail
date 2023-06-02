bb75d08f57f0c4bd87ea66382f91ec269bd2974f
Ziad Abouelfarah <zain.fake02@gmail.com>
2023-06-03 00:45:45 +0100

    f

--- src/pages/program/[name].tsx
+++ src/pages/program/[name].tsx
@@ -5,9 +5,18 @@ import Head from "next/head";
 
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+import { useRouter } from "next/router";
+import { getProgram } from "@/actions/programs";
+import { useQuery } from "react-query";
 
 const Me: NextPage = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const router = useRouter();
+  const { name } = router.query;
+  const result = useQuery({
+    queryKey: ["program", name],
+    queryFn: () => getProgram(name as string),
+  });
 
   return (
     <>
@@ -16,15 +25,18 @@ const Me: NextPage = () => {
       </Head>
       <NavBar />
       <Container>
+        <h1 className="text-center font-sans text-lg mb-8">
+          {result.data?.data.title}
+        </h1>
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.7.107/build/pdf.worker.js">
           <div style={{ height: "750px" }}>
-            <Viewer
-              httpHeaders={{ "Access-Control-Allow-Origin": "*" }}
-              fileUrl={
-                "https://firebasestorage.googleapis.com/v0/b/shen-6832a.appspot.com/o/LUS-Recovered_compressed.pdf?alt=media&token=2884a38b-181c-4ed9-b882-17da2b3c5338"
-              }
-              plugins={[defaultLayoutPluginInstance]}
-            />
+            {result.isSuccess ? (
+              <Viewer
+                httpHeaders={{ "Access-Control-Allow-Origin": "*" }}
+                fileUrl={result.data?.data.pdf_link}
+                plugins={[defaultLayoutPluginInstance]}
+              />
+            ) : null}
           </div>
         </Worker>
       </Container>
